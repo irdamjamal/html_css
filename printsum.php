818e558968c8e7be47897964301e30bb6944bf60
Irdam Jamal <107344212+irdamjamal@users.noreply.github.com>
2024-04-30 18:58:16 +0530

    Add files via upload

---
+++ printsum.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+        $x = 10;
+        $y = 6;
+        echo("<br>the value of x =".$x);
+        echo("<br>the value of x =".$y);
+        echo("<br>addition is =".($x + $y));
+        ECHO "hello world!<br>";
+        EcHo "hello world!<br>";
+        echo "hello world!<br>";
+
+        ?>
+    </body>
+</html>
